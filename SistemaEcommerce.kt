eded7d3f25afdfb9d1e6075bc971a927d9b80293
oliveiranatalia <102336871+oliveiranatalia@users.noreply.github.com>
2022-05-10 15:22:58 -0300

    editar main

--- SistemaEcommerce.kt
+++ SistemaEcommerce.kt
@@ -3,7 +3,6 @@ import src.menu.Menu
 
 fun main() {
     println("Boas vindas ao Seu Estoque em dia!")
-    val estoque: Estoque = Estoque()
+    val estoque = Estoque()
     Menu().menuPrincipal(estoque)
 }
-
