6a256fc7871b406085de3220b51caa51e50dda3e
oliveiranatalia <102336871+oliveiranatalia@users.noreply.github.com>
2022-05-10 15:24:56 -0300

    editar rmenu

--- src/menu/Menu.kt
+++ src/menu/Menu.kt
@@ -5,35 +5,39 @@ import kotlin.system.exitProcess
 
 class Menu {
     fun menuPrincipal(estoque: Estoque) {
-
-        println("O que deseja fazer?")
-        println("1 - Cadastrar novo item")
-        println("2 - Ver lista de itens disponíveis para compra")
-        println("3 - Dar baixa em um item")
-        println("4 - Sair do programa")
-
-        //TODO:deixar sem o try/catch para avaliar se a galera percebe que dá problema de entrada de dado se for string
-        val opcao = readln().toInt()
-
-        when (opcao) {
-            1 -> {
-                estoque.registrarItem()
-                this.menuPrincipal(estoque)
+        try{
+            println("O que deseja fazer?")
+            println("1 - Cadastrar novo item")
+            println("2 - Ver lista de itens disponíveis para compra")
+            println("3 - Dar baixa em um item")
+            println("4 - Verificar itens repetidos")
+            println("5 - Sair do programa")
+            when (readln().toInt()) {
+                1 -> {
+                    estoque.registrarItem()
+                    this.menuPrincipal(estoque)
+                }
+                2 -> {
+                    estoque.listarItens()
+                    this.menuPrincipal(estoque)
+                }
+                3 -> {
+                    estoque.darBaixaItem()
+                    this.menuPrincipal(estoque)
+                }
+                4 -> {
+                    estoque.verificarItemRepetido()
+                    this.menuPrincipal(estoque)
+                }
+                5 -> exitProcess(10)
+                else -> {
+                    println("Opção inválida, tente novamente\n")
+                    menuPrincipal(estoque)
+                }
             }
-            2 -> {
-                estoque.listarItens()
-                this.menuPrincipal(estoque)
-            }
-            3 -> {
-                estoque.darBaixaItem()
-                this.menuPrincipal(estoque)
-            }
-
-            4 -> exitProcess(10)
-            else -> {
-                println("Opção inválida, tente novamente")
-                menuPrincipal(estoque);
-            }
+        }catch(e: NumberFormatException){
+            println("Opção inválida, tente novamente\n")
+            menuPrincipal(estoque)
         }
     }
 }
