b461d64c1418c7ad12e478d0a1c128e1e2c84b03
oliveiranatalia <102336871+oliveiranatalia@users.noreply.github.com>
2022-05-10 15:24:16 -0300

    editar item

--- src/item/Item.kt
+++ src/item/Item.kt
@@ -1,12 +1,6 @@
 package src.item
 
-import java.math.BigDecimal
-
-//TODO: Talvez remover os parâmetros do construtor
-class Item(val codigo: String, private val nome: String, private val preco: BigDecimal) {
-
-    protected val valorDesconto: String = "";
-
+class Item(val codigo: Int, val nome: String, val preco: Double) {
     override fun toString(): String {
         return "código: $codigo | nome: $nome | preço(R$) $preco";
     }
